0f6ae2aa5f4c79d8520b7e66d8fb590effbbbd39
log-17 <645927031@qq.com>
2020-09-08 14:23:19 +0800

    realise store menu map

--- src/main/java/com/example/web/session/OperatorSessionHandler.java
+++ src/main/java/com/example/web/session/OperatorSessionHandler.java
@@ -16,8 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Logger;
 
 @Service
@@ -35,14 +34,31 @@ public class OperatorSessionHandler {
 
     public OperatorSession createOperatorSession(HttpServletRequest request, HttpServletResponse response, Operator operator) throws Exception {
         logger.info("＝＝＝创建操作员会话＝＝＝");
+        // 根据操作员职位查找可用的所有菜单
         Position position = positionMapper.selectPositionByOperatorId(operator.getId());
         List<Menu> menuList = menuMapper.selectRelatedMenu(position.getPositionId());
+
+        Map<Menu, List<Menu>> menuMap = new HashMap<>();
+        for (int i = 0; i < menuList.size(); i++) {
+            // 如果是父菜单
+            if (menuList.get(i).getParentMenuId() == -1) {
+                Integer parentId = menuList.get(i).getMenuId();
+                List<Menu> childMenuList = new ArrayList<>();
+                // 将同一父菜单的子菜单存入 childMenuList 并放入具有相同 key 的 menuMap 中
+                for (int j = 0; j < menuList.size(); j++) {
+                    if (menuList.get(j).getParentMenuId() == parentId) {
+                        childMenuList.add(menuList.get(j));
+                    }
+                }
+                menuMap.put(menuMapper.findMenuById(parentId), childMenuList);
+            }
+        }
 
         DateFormat df = new SimpleDateFormat(DATE_FORMAT);
 
         OperatorSession operatorSession = new OperatorSession();
         operatorSession.setSessionId(request.getSession().getId());
-        operatorSession.setMenuList(menuList);
+        operatorSession.setMenuMap(menuMap);
         operatorSession.setToken(ConfigTools.encrypt(operator.getId().toString()));
         operatorSession.setThisLoginTime(df.format(new Date()));
         operatorSession.setLastLoginTime(df.format(new Date()));
